72bb0fa3ee23dad81f83249580a17d664349b49a
Eduardo Oliveira <eduolialmeida@gmail.com>
2021-05-31 19:50:50 -0300

    Cria artistas.bin - Modifica artistas.csv main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 using namespace std;
 
-// ********** atual
+#define MAX_NAME 512
+#define MAX_ID 50
+#define MAX_FOLLOWERS 10
+#define MAX_POPULARITY 10
+
 
 struct Artista
 {
-    string id;
-    string followers;
-    string name;
-    string popularity;
+    char id[MAX_ID]; 
+    float followers;
+    char name[MAX_NAME]; 
+    int popularity;
 };
 
+
+void Artista_Save(Artista &art, fstream &fileOut) 
+{
+    fileOut.write(art.id, sizeof(art.id));
+    fileOut.write((char*)&art.followers, sizeof(art.followers));
+    fileOut.write(art.name, sizeof(art.name));
+    fileOut.write((char*)&art.popularity, sizeof(art.popularity));
+}
+
+void Artista_Read(Artista &art, fstream &fileOut) 
+{
+    fileOut.read(art.id, sizeof(art.id));
+    fileOut.read((char*)&art.followers, sizeof(art.followers));
+    fileOut.read(art.name, sizeof(art.name));
+    fileOut.read((char*)&art.popularity, sizeof(art.popularity));
+}
+
+void Imprime_Artista(Artista &art)
+{
+    cout << art.id << endl;
+    cout << art.name << endl;
+    cout << art.popularity << endl;
+    cout << art.followers << endl;
+}
 
 int main()
 {
@@ -20,12 +49,29 @@ int main()
     fileIn.open("artistas.csv");
     Artista art1;
 
+
     if(fileIn.is_open())
     {
-        getline(fileIn, art1.id, ',');
-        getline(fileIn, art1.followers, ',');
-        getline(fileIn, art1.name, ',');
-        getline(fileIn, art1.popularity, '\n');
+        string tmp;
+
+        getline(fileIn, tmp, ',');
+        strcpy(art1.id, tmp.c_str());
+
+        getline(fileIn, tmp, ',');
+        //strcpy(art1.followers, tmp.c_str());
+        art1.followers = atof(tmp.c_str());
+
+        cout << "*******" << tmp.c_str() << endl;
+        cout << "*******" << art1.followers << endl;
+
+        getline(fileIn, tmp, ',');
+        strcpy(art1.name, tmp.c_str());
+
+        getline(fileIn, tmp, '\n');
+        //strcpy(art1.popularity, tmp.c_str());
+        art1.popularity = atoi(tmp.c_str());
+
+        Imprime_Artista(art1);
 
         fileIn.close();
     }
@@ -36,12 +82,12 @@ int main()
     } 
 
     //pega o q tá no struct e salva em arquivo binário
-    ofstream fileOut;
-    fileOut.open("artistas.dat", ios::out | ios::binary);
+    fstream fileOut;
+    fileOut.open("artistas.bin", ios::out | ios::binary); 
 
     if(fileOut.is_open())
     {
-        fileOut.write((char*)(&art1), sizeof(Artista));
+        Artista_Save(art1, fileOut);
         fileOut.close();
     }
     else
@@ -51,14 +97,14 @@ int main()
     } 
 
     //lê o que tá no arquivo binário artistas.dat
-    ifstream fileI;
-    fileI.open("artistas.dat", ios::in | ios::binary);
+    fstream fileI;
+    fileI.open("artistas.bin", ios::in | ios::binary);
 
     if(fileI.is_open())
     {
         Artista art;
 
-        fileI.read((char*)(&art), sizeof(Artista));
+        Artista_Read(art, fileI);
 
         cout << "\nLendo dados no arquivo em binario: " << endl;
         cout << "id: " << art.id << endl;
