9023c9685ae5e220c971c59d3a11b2122e18b26b
Eduardo Oliveira <eduolialmeida@gmail.com>
2021-06-02 10:04:52 -0300

    Modifica main.cpp - Deleta artistas.dat, teste

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ struct Artista
 };
 
 
-void Artista_Save(Artista &art, fstream &fileOut) 
+void artista_Save(Artista &art, fstream &fileOut) 
 {
     fileOut.write(art.id, sizeof(art.id));
     fileOut.write((char*)&art.followers, sizeof(art.followers));
@@ -26,7 +26,7 @@ void Artista_Save(Artista &art, fstream &fileOut)
     fileOut.write((char*)&art.popularity, sizeof(art.popularity));
 }
 
-void Artista_Read(Artista &art, fstream &fileOut) 
+void artista_Read(Artista &art, fstream &fileOut) 
 {
     fileOut.read(art.id, sizeof(art.id));
     fileOut.read((char*)&art.followers, sizeof(art.followers));
@@ -34,12 +34,12 @@ void Artista_Read(Artista &art, fstream &fileOut)
     fileOut.read((char*)&art.popularity, sizeof(art.popularity));
 }
 
-void Imprime_Artista(Artista &art)
+void imprime_Artista(Artista &art)
 {
-    cout << art.id << endl;
-    cout << art.name << endl;
-    cout << art.popularity << endl;
-    cout << art.followers << endl;
+    cout << "id: " << art.id << endl;
+    cout << "followers: " <<  art.followers << endl;
+    cout << "name: " << art.name << endl;
+    cout << "popularity: " << art.popularity << endl;
 }
 
 int main()
@@ -61,9 +61,6 @@ int main()
         //strcpy(art1.followers, tmp.c_str());
         art1.followers = atof(tmp.c_str());
 
-        cout << "*******" << tmp.c_str() << endl;
-        cout << "*******" << art1.followers << endl;
-
         getline(fileIn, tmp, ',');
         strcpy(art1.name, tmp.c_str());
 
@@ -71,7 +68,7 @@ int main()
         //strcpy(art1.popularity, tmp.c_str());
         art1.popularity = atoi(tmp.c_str());
 
-        Imprime_Artista(art1);
+        imprime_Artista(art1);
 
         fileIn.close();
     }
@@ -87,7 +84,7 @@ int main()
 
     if(fileOut.is_open())
     {
-        Artista_Save(art1, fileOut);
+        artista_Save(art1, fileOut);
         fileOut.close();
     }
     else
@@ -96,30 +93,7 @@ int main()
         exit(1);
     } 
 
-    //lê o que tá no arquivo binário artistas.dat
-    fstream fileI;
-    fileI.open("artistas.bin", ios::in | ios::binary);
-
-    if(fileI.is_open())
-    {
-        Artista art;
-
-        Artista_Read(art, fileI);
-
-        cout << "\nLendo dados no arquivo em binario: " << endl;
-        cout << "id: " << art.id << endl;
-        cout << "followers: " << art.followers << endl;
-        cout << "name: " << art.name << endl;
-        cout << "popularity: " << art.popularity << endl;
-
-        fileI.close();
-    }
-    else
-    {
-        cout << "Erro" << endl;
-        exit(1);
-    } 
-
+   
 
 
 return 0;
